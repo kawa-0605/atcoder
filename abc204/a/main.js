b004ad794a70d5f956a26af4192f87513bf3f246
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-27 13:50:27 +0900

    js: abc204-a

--- abc204/a/main.js
+++ abc204/a/main.js
@@ -1,3 +1,5 @@
 function Main(input) {
+  const[a, b] = input.trim().split('\n')[0].split(' ').map(Number)
+  console.log(a === b ? a : 3 - a - b)
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
