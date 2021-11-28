d94a287ec53c601437de409640798b084de7eed5
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-28 11:19:38 +0900

    js abc169-c

--- abc169/c/main.js
+++ abc169/c/main.js
@@ -1,3 +1,8 @@
 function Main(input) {
+  let [a, b] = input.split('\n')[0].split(' ')
+  a = BigInt(a)
+  b = BigInt(b.replace('.', ''))
+  let ans = a * b / 100n
+  console.log(ans.toString())
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
