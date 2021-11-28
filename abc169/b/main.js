5582a586de25d6a632c0f8d41b384c0241559b1d
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-28 11:19:25 +0900

    js: abc169-b

--- abc169/b/main.js
+++ abc169/b/main.js
@@ -1,3 +1,17 @@
 function Main(input) {
+  let arr = input.split('\n')[1].split(' ').map(Number).sort()
+  const max = BigInt(10 ** 18)
+
+  let ans = 1n
+  for(num of arr) {
+    ans *= BigInt(num)
+    if (ans > max) {
+      ans = -1
+      break
+    }
+  }
+
+  console.log(ans.toString())
+
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
