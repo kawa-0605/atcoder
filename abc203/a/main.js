0afafe980b5208e8b5c92c492d0cc4a07389ecb4
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:44:00 +0900

    js: abc203-a

--- abc203/a/main.js
+++ abc203/a/main.js
@@ -1,3 +1,15 @@
-function Main(input) {
+function main(input) {
+    const [a, b, c] = input.trim().split(" ")
+    
+    if (a === b) {
+        console.log(c)
+    } else if (b === c) {
+        console.log(a)
+    } else if (a === c) {
+        console.log(b)
+    } else {
+        console.log(0)
+    }
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
