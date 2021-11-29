aac454b3d399e5c3d08442bae319413faa6ee76d
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc178-a

--- abc178/a/main.js
+++ abc178/a/main.js
@@ -1,3 +1,14 @@
-function Main(input) {
+function main(input) {
+    const a = input.trim().split(" ").map(Number)
+
+    switch (a[0]) {
+        case 0:
+            console.log(1)
+            break
+        case 1:
+            console.log(0)
+            break
+    }
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
