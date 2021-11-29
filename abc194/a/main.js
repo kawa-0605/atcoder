0fe78a883f0828928fbda99af4fe7a557471a200
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:44:00 +0900

    js: abc194-a

--- abc194/a/main.js
+++ abc194/a/main.js
@@ -1,3 +1,17 @@
-function Main(input) {
+function main(input) {
+    const [a, b] = input.trim().split(" ").map(Number)
+
+    const c = a + b
+
+    if (c >= 15 && b >= 8) {
+        console.log(1)
+    } else if (c >= 10 && b >= 3) {
+        console.log(2)
+    } else if (c >= 3) {
+        console.log(3)
+    } else {
+        console.log(4)
+    }
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
