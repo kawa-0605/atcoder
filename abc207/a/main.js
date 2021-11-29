2f4245d0b02d4f33edd63578caeb733a66f455b4
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:44:06 +0900

    js: abc207-a

--- abc207/a/main.js
+++ abc207/a/main.js
@@ -1,3 +1,12 @@
-function Main(input) {
+function main(input) {
+    let arr = input.trim().split(" ").map(Number)
+    let ans = 0
+
+    arr = arr.sort(function(a, b) {
+        return b - a
+    })
+    ans = arr[0] + arr [1]
+    console.log(ans)
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
