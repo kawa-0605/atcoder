bf39a1890ed85b8c672b9f5c50920cb50277607a
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc155-b

--- abc005/b/main.js
+++ abc005/b/main.js
@@ -1,3 +1,8 @@
-function Main(input) {
+function main(input) {
+    const arr = input.split('\n').slice(1, -1).map(n => Number(n))
+
+    arr.sort((a, b) => a - b)
+    console.log(arr[0])
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
