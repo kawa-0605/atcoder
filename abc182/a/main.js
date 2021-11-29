5663858fddf39b20741b68d17ab3e6ee647adca9
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc182-a

--- abc182/a/main.js
+++ abc182/a/main.js
@@ -1,3 +1,8 @@
-function Main(input) {
+function main(input) {
+    const [a, b] = input.trim().split(" ").map(Number)
+
+    const ans = 2 * a + 100 - b
+    console.log(ans)
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
