4945ef77062fa2d5ef8af60e1235ee0207675c07
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc172-b

--- abc172/b/main.js
+++ abc172/b/main.js
@@ -1,3 +1,15 @@
-function Main(input) {
+function main(input) {
+    const [s, t] = input.split("\n")
+    let cnt = 0;
+
+    if (s !== t) {
+       for (let i = 0; i <= s.length; i++) {
+           if (s.charAt(i) !== t.charAt(i)) {
+               cnt++
+           }
+       } 
+    }
+    console.log(cnt)
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
