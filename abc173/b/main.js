3a2a38375ef220c8dc6fc948a948de096a4b0e45
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc173-b

--- abc173/b/main.js
+++ abc173/b/main.js
@@ -1,3 +1,33 @@
-function Main(input) {
+function main(input) {
+    const arr = input.split("\n").slice(1)
+    let ac = 0
+    let wa = 0
+    let tle = 0
+    let re = 0
+
+    for (key of arr) {
+        switch(key) {
+            case 'AC':
+                ac++
+                break
+            case 'WA':
+                wa++
+                break
+            case 'TLE':
+                tle++
+                break
+            case 'RE':
+                re++
+                break
+        }
+    }
+
+    console.log(`
+        AC x ${ac}
+        WA x ${wa}
+        TLE x ${tle}
+        RE x ${re}
+    `)
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
