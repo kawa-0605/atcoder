df453c270f68f078cf9badcc8f35f5b21e3e44e2
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc164-a

--- abc164/a/main.js
+++ abc164/a/main.js
@@ -1,3 +1,11 @@
-function Main(input) {
+function main(input) {
+    const [a, b] = input.trim().split(" ").map(Number)
+    
+    if (a > b) {
+        console.log('safe')
+    } else {
+        console.log('unsafe')
+    }
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
