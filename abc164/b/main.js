be59f01ac52a3f9ecbd8ae4fd880a928c3f3ab36
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-11-29 22:43:59 +0900

    js: abc164-b

--- abc164/b/main.js
+++ abc164/b/main.js
@@ -1,3 +1,17 @@
-function Main(input) {
+function main(input) {
+    let [t_hp, t_at, a_hp, a_at] = input.trim().split(" ").map(Number)
+    let turn = 0
+
+    while(t_hp > 0 && a_hp > 0) {
+        if (turn % 2 === 0) {
+            a_hp -= t_at
+        } else {
+            t_hp -= a_at
+        }
+        turn++
+    }
+
+    console.log(t_hp > 0 ? 'Yes' : 'No')
 }
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"))
