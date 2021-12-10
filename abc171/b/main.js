35d89f6e9a25be1751ea7d53ba2716edc7c71859
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-12-10 16:53:56 +0900

    js: abc171-b

--- abc171/b/main.js
+++ abc171/b/main.js
@@ -1,3 +1,15 @@
 function Main(input) {
+  let f = function(a, b) {
+    return a - b
+  }
+  let ans = 0
+  // maxTypeいらんかった...
+  let [maxType, selectionType] = input.split('\n')[0].split(' ').map(Number)
+  let priceArr = input.split('\n')[1].split(' ').map(Number).sort(f)
+
+  for (let i = 0; i < selectionType; i++){
+    ans += priceArr[i]
+  }
+  console.log(ans)
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
