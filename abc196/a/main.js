0213a8192c20890e9dee017918962588d27c0950
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-12-11 13:07:14 +0900

    js: abc196-a

--- abc196/a/main.js
+++ abc196/a/main.js
@@ -1,3 +1,6 @@
 function Main(input) {
+  const [a, b] = input.split('\n')[0].split(' ').map(Number)
+  const [c, d] = input.split('\n')[1].split(' ').map(Number)
+  console.log(b - c)
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
