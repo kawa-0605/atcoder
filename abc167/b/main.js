4cb050bf60293f012ac51d7afc59b13363f035b9
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-12-11 11:44:49 +0900

    js: abc167-b

--- abc167/b/main.js
+++ abc167/b/main.js
@@ -1,3 +1,12 @@
 function Main(input) {
+  let [a, b, c, k] = input.split('\n')[0].split(' ').map(Number)
+
+  if (k <= a) {
+    console.log(k)
+  } else if (k - a <= b) {
+    console.log(a)
+  } else {
+    console.log(a - (k - a - b))
+  }
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
