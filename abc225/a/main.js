4b86c1e5b3b483dbc3c8fc3d5849c50879e80533
y.kawamura <mstkzr0605@yahoo.co.jp>
2022-01-15 19:52:23 +0900

    js: abc225-a

--- abc225/a/main.js
+++ abc225/a/main.js
@@ -1,3 +1,9 @@
 function Main(input) {
+  input = input.split('\n')[0].split('')
+  const a = [...new Set(input)];
+  console.log(
+    a.length == 1 ? 1 :
+    a.length == 2 ? 3 : 6
+  )
 }
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
